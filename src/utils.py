3d72c2e5693afdd4c8c8f0a3df578716881b4258
SolidData <david.tiefenthaler@web.de>
2024-11-02 13:00:58 +0100

    add get_embedding_size

--- src/utils.py
+++ src/utils.py
@@ -246,3 +246,20 @@ def calculate_weighted_cost(
         )
 
     return round(cost, 2)
+
+
+def get_embedding_size(n: int, max_size: int = 100) -> int:
+    """
+    Determine empirically good embedding sizes (formula taken from fastai).
+
+    Args:
+        n (int): number of classes
+        max_size (int, optional): maximum embedding size. Defaults to 100.
+
+    Returns:
+        int: embedding size
+    """
+    if n > 2:
+        return min(round(1.6 * n**0.56), max_size)
+    else:
+        return 1
