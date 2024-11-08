9bc8043af923b87cd4287c571e0c7e45b5d49699
SolidData <david.tiefenthaler@web.de>
2024-11-08 19:00:40 +0100

    restructure src code base modularity

---
+++ src/tabular_lightning/tabular_models.py
@@ -0,0 +1,156 @@
+# Pytorch Models for Tabular Data.
+
+from typing import Dict, Iterable, List, Literal, Optional, Tuple, Union
+
+import torch
+from torch import nn
+
+class MulticlassTabularMLP(nn.Module):
+    def __init__(
+        self,
+        input_size: int = None,
+        output_size: int = None,
+        hidden_size: int = None,
+        n_hidden_layers: int = None,
+        activation_class: nn.Module = nn.ReLU,
+        dropout: float = None,
+        norm: bool = True,
+    ) -> None:
+        """Multi Layer Perceptron (MLP) for multiclass classification for tabular data.
+        Args:
+            input_size (int): Number of input features.
+            output_size (int): Number of output classes.
+            hidden_size (int): Number of neurons in hidden layers.
+            n_hidden_layers (int): Number of hidden layers.
+            activation_class (nn.Module): Activation function.
+            dropout (float): Dropout rate.
+            norm (bool): Whether to use layer normalization.
+        """
+        super().__init__()
+        self.input_size = input_size
+        self.output_size = output_size
+        self.hidden_size = hidden_size
+        self.n_hidden_layers = n_hidden_layers
+        self.activation_class = activation_class
+        self.dropout = dropout
+        self.norm = norm
+
+        ### define MLP ###
+        # input layer
+        module_list = [nn.Linear(input_size, hidden_size), activation_class()]
+        if dropout is not None:
+            module_list.append(nn.Dropout(dropout))
+        if norm:
+            module_list.append(nn.LayerNorm(hidden_size))
+        # hidden layers
+        for _ in range(n_hidden_layers):
+            module_list.extend([nn.Linear(hidden_size, hidden_size), activation_class()])
+            if dropout is not None:
+                module_list.append(nn.Dropout(dropout))
+            if norm:
+                module_list.append(nn.LayerNorm(hidden_size))
+        # output layer
+        module_list.append(nn.Linear(hidden_size, output_size))
+
+        self.sequential = nn.Sequential(*module_list)
+
+    def forward(self, x: Dict[str, torch.Tensor]) -> torch.Tensor:
+        """Forward pass through the MLP."""
+        # concatenate continuous and categorical features
+        network_input = torch.cat((x["continuous"], x["categorical"]), dim=1) # NOTE: converts all data types to float32 (respective to the data type of the first element)
+        return self.sequential(network_input)
+
+
+class MulticlassTabularCatEmbeddingMLP(nn.Module):
+    def __init__(
+        self,
+        continuous_cols: List[str] = None,
+        categorical_cols: List[str] = None,
+        output_size: int = None,
+        # embedding_dim: int = None,
+        hidden_size: int = None,
+        n_hidden_layers: int = None,
+        activation_class: nn.Module = nn.ReLU,
+        dropout: float = None,
+        norm: bool = True,
+        embedding_sizes: Dict[str, Tuple[int, int]] = None,
+    ) -> None:
+        """Embedding Multi Layer Perceptron (embMLP) with embedding for categorical features for multiclass classification for tabular data.
+        Args:
+            continues_cols (List[str]): order of continuous variables in tensor passed to forward function.
+            categorical_cols (List[str]): order of categorical variables in tensor passed to forward function.
+            output_size (int): Number of output classes.
+            hidden_size (int): Number of neurons in hidden layers.
+            n_hidden_layers (int): Number of hidden layers.
+            activation_class (nn.Module): Activation function.
+            dropout (float): Dropout rate.
+            norm (bool): Whether to use layer normalization.
+            embedding_sizes (Dict[str, Tuple[int, int]]): Dictionary of embedding sizes for each categorical feature.
+        """
+        super().__init__()
+        self.continuous_cols = continuous_cols
+        self.categorical_cols = categorical_cols
+        self.output_size = output_size
+        self.embedding_sizes = embedding_sizes
+        # self.embedding_dim = embedding_dim
+        self.hidden_size = hidden_size
+        self.n_hidden_layers = n_hidden_layers
+        self.activation_class = activation_class
+        self.dropout = dropout
+        self.norm = norm
+
+        ### define the Embedding MLP ###
+        ## embedding layers
+        # cont featurres
+        self.cont_normalizing = nn.BatchNorm1d(len(self.continuous_cols))
+        # cat features
+        self.cat_embeddings = nn.ModuleDict()
+        for name in embedding_sizes.keys():
+            self.cat_embeddings[name] = nn.Embedding(
+                embedding_sizes[name][0],
+                embedding_sizes[name][1],
+            )
+        ## input layer mlp
+        mlp_input_size = sum(value[1] for value in embedding_sizes.values()) + len(self.continuous_cols)
+        module_list = [nn.Linear(mlp_input_size, hidden_size), activation_class()]
+        if dropout is not None:
+            module_list.append(nn.Dropout(dropout))
+        if norm:
+            module_list.append(nn.LayerNorm(hidden_size))
+        ## hidden layers
+        for _ in range(n_hidden_layers):
+            module_list.extend([nn.Linear(hidden_size, hidden_size), activation_class()])
+            if dropout is not None:
+                module_list.append(nn.Dropout(dropout))
+            if norm:
+                module_list.append(nn.LayerNorm(hidden_size))
+        ## output layer
+        module_list.append(nn.Linear(hidden_size, output_size))
+
+        self.mlp_layers = nn.Sequential(*module_list)
+
+    def forward(self, x: Dict[str, torch.Tensor]) -> torch.Tensor:
+        """Forward pass through the embMLP."""
+
+        assert "continuous" in x or "categorical" in x, "x must contain either continuous or categorical features"
+
+        ### forward embedding layers ###
+        # cont features
+        if len(self.continuous_cols) > 0:
+            embed_vector_cont = self.cont_normalizing(x["continuous"])
+        else:
+            embed_vector_cont = x["continuous"]
+        # cat features
+        if len(self.categorical_cols) > 0:
+            output_vectors = {}
+            for idx, (name, emb) in enumerate(self.cat_embeddings.items()):
+                output_vectors[name] = emb(x["categorical"][:, idx])
+            embed_vector_cat = torch.cat(list(output_vectors.values()), dim=1)
+        # output_vector_emded
+        if embed_vector_cont is None:
+            output_vector_emded = embed_vector_cat
+        else:
+            output_vector_emded = torch.cat([embed_vector_cont, embed_vector_cat], dim=1)
+
+        ### forward hidden layers ###
+        return self.mlp_layers(output_vector_emded)
